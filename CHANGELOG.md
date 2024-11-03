aeea4179ae8326503b5c367cd9f834fa2eafb177
Joseph Lewis III <joseph@josephlewis.net>
2024-11-02 21:32:29 -0700

    Added syntax highlighting

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- Syntax highlighting matching DevDocs. (#30)
+
 ### Changed
 
 - Page navigation is now dynamically rendered reducing file sizes. (#24, #31)
--- zimui/src/main.ts
+++ zimui/src/main.ts
@@ -1,5 +1,6 @@
 import { defineCustomElement } from 'vue'
 import Navbar from './components/DevdocsNavbar.vue'
+import './highlighter'
 
 // Convert the navbar into a custom element. We're using this rather than
 // the shorthand .ce.vue naming because we need to disable shadowRoot
